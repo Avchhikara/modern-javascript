0eb8846e152ddc5ec59754f6d51a639862c32bd1
Avnish <Avnishway@gmail.com>
2018-12-06 06:52:01 +0530

    Async and Await

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,29 +1,7 @@
-document.getElementById("button1").addEventListener("click", getText);
-document.getElementById("button2").addEventListener("click", getJSON);
-document.getElementById("button3").addEventListener("click", getAPI);
-
-function getText() {
-  fetch("text.txt")
-    .then(res => res.text())
-    .then(data => (document.querySelector(".output").textContent = data));
+async function myfunc() {
+  const res = await fetch("https://api.github.com/users");
+  const data = await res.json();
+  return data;
 }
 
-function getJSON() {
-  let out = "";
-  fetch("customers.json")
-    .then(res => res.json())
-    .then(data => {
-      data.forEach(val => (out += `<li>${val.name}</li>`));
-      document.querySelector(".output").innerHTML = out;
-    });
-}
-
-function getAPI() {
-  let out = "";
-  fetch("https://api.github.com/users")
-    .then(res => res.json())
-    .then(data => {
-      data.forEach(val => (out += `<li>${val.login}</li>`));
-      document.querySelector(".output").innerHTML = out;
-    });
-}
+myfunc().then(res => console.log(res));
